99221d63ede102c980555e97c4487dbb8ff5c332
WORMSS <wormssmail@gmail.com>
2021-04-10 23:13:48 +0100

    Added tests and renamed internal variables

--- src/lib/supplier/createWalker.ts
+++ src/lib/supplier/createWalker.ts
@@ -4,12 +4,18 @@ export function createWalker<T>(
   node: T,
   childrenMapper: WalkerChildren<T>,
 ): Generator<T, any, undefined> {
+  if (node === null || node === undefined) {
+    throw new TypeError('expected node to not be null');
+  }
+  if (!(typeof childrenMapper === 'function')) {
+    throw new TypeError('expected childrenMapper to be a function');
+  }
   return walker(node, childrenMapper, new Set());
 }
 
 function* walker<T>(
   node: T,
-  children: WalkerChildren<T>,
+  childrenMapper: WalkerChildren<T>,
   visited: Set<T>,
 ): Generator<T, any, undefined> {
   if (visited.has(node)) {
@@ -17,10 +23,10 @@ function* walker<T>(
   }
   visited.add(node);
   yield node;
-  const childrenNodes = children(node);
-  if (childrenNodes !== null && childrenNodes !== undefined) {
-    for (const child of childrenNodes) {
-      yield* walker(child, children, visited);
+  const children = childrenMapper(node);
+  if (children !== null && children !== undefined) {
+    for (const child of children) {
+      yield* walker(child, childrenMapper, visited);
     }
   }
 }
