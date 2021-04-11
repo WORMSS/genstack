221fcdaf9abfb2a5941b18643de949fcdf3f086b
WORMSS <wormssmail@gmail.com>
2021-04-11 22:46:00 +0100

    Changed error to type error

--- src/lib/guards/assertIterator.ts
+++ src/lib/guards/assertIterator.ts
@@ -2,6 +2,6 @@ import { isIterator } from './isIterator';
 
 export function assertIterator<T>(obj: unknown): asserts obj is Iterator<T> {
   if (!isIterator(obj)) {
-    throw new Error(`${String(obj)} is not an Iterator`);
+    throw new TypeError(`${String(obj)} is not an Iterator`);
   }
 }
